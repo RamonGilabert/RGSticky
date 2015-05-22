c3707e397a040cb5985e120ec3836b0e7b47f61b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-22 21:37:15 +0200

    Added second image

--- Source/RGStickyTableView.swift
+++ Source/RGStickyTableView.swift
@@ -2,11 +2,21 @@ import UIKit
 
 class RGStickyTableView: UITableView {
 
+    var height: CGFloat?
+    var imageView: UIImageView {
+        set {
+            self.imageView.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y - self.height!, self.frame.width, self.height!)
+            self.imageView.contentMode = UIViewContentMode.ScaleAspectFill
+            self.imageView.clipsToBounds = true
+            setNeedsDisplay()
+        } get {
+            return self.imageView
+        }
+    }
+
     func addStickyImage(image: UIImage, heigth: CGFloat) {
-        let imageView = UIImageView(frame: CGRectMake(self.frame.origin.x, self.frame.origin.y - heigth, self.frame.width, heigth))
-        imageView.image = image
-        imageView.contentMode = UIViewContentMode.ScaleAspectFill
-        imageView.clipsToBounds = true
+        self.height = heigth
+        self.imageView.image = image
         
         self.contentInset = UIEdgeInsetsMake(heigth, 0, 0, 0)
         self.addSubview(imageView)
