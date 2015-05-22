b38f6daa97824623a3a949d5e729445d7304bfe9
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-22 21:19:14 +0200

    Added some code to add image

--- Source/RGStickyTableView.swift
+++ Source/RGStickyTableView.swift
@@ -2,8 +2,12 @@ import UIKit
 
 class RGStickyTableView: UITableView {
 
-    func frameWithBackgroundStickyImage(imageView: UIImageView) {
-        self.contentInset = UIEdgeInsetsMake(imageView.frame.height, 0, 0, 0)
+    func addStickyImage(image: UIImage, heigth: CGFloat) {
+        let imageView = UIImageView(frame: CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.width, heigth))
+        imageView.image = image
+        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        
+        self.contentInset = UIEdgeInsetsMake(heigth, 0, 0, 0)
         self.addSubview(imageView)
     }
 
