fb1b4a8b6a58decf0a49f7ee21fbdc6a00a6290d
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-22 20:52:53 +0200

    Adding some code

--- Source/RGStickyTableView.swift
+++ Source/RGStickyTableView.swift
@@ -2,8 +2,10 @@ import UIKit
 
 class RGStickyTableView: UITableView {
 
-    func frameWithBackgroundStickyImage(frame: CGRect, image: UIImage) {
+    func frameWithBackgroundStickyImage(frame: CGRect, imageView: UIImageView) {
         self.frame = CGRectMake(frame.origin.x, frame.origin.y, frame.width, frame.height)
+        self.contentInset = UIEdgeInsetsMake(imageView.frame.height, 0, 0, 0)
+        self.addSubview(imageView)
     }
 
     func scrollViewDidScroll(scrollView: UIScrollView) {
