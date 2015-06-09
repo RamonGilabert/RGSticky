30542add90deb5b83cfadc11f91b017a63482c34
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-06-08 21:52:24 -0700

    Added some dummy data

--- Examples/RGStickyExamples/RGStickyExamples/DataManager.swift
+++ Examples/RGStickyExamples/RGStickyExamples/DataManager.swift
@@ -1,5 +1,47 @@
 import Foundation
 
-struct RandomData {
-    static let ""
+struct Data {
+
+    // MARK: Images
+
+    static let OneImage = "one-image"
+    static let TwoImage = "two-image"
+    static let ThreeImage = "three-image"
+    static let FourImage = "four-image"
+    static let FiveImage = "five-image"
+    static let SixImage = "six-image"
+    static let SevenImage = "seven-image"
+    static let EightImage = "eight-image"
+    static let NineImage = "nine-image"
+    static let TenImage = "ten-image"
+
+    // MARK: Names
+
+    static let OneName = "Rebecca Lawrence"
+    static let TwoName = "Michael Payne"
+    static let ThreeName = "Shirley Patel"
+    static let FourName = "Carl Ward"
+    static let FiveName = "Walter Perez"
+    static let SixName = "Christian Phillips"
+    static let SevenName = "Julia Richards"
+    static let EightName = "Paul Woods"
+    static let NineName = "Kathleen Jordan"
+    static let TenName = "Patricia Matthews"
+
+    // MARK: Comments
+
+    static let OneComment = "Fap slow-carb migas, listicle direct trade readymade church-key lomo master cleanse."
+    static let TwoComment = "Put a bird on it quinoa fashion axe, cray hashtag retro irony hoodie actually Marfa."
+    static let ThreeComment = "Synth locavore heirloom direct trade tofu, cliche migas Etsy Williamsburg try-hard."
+    static let FourComment = "Helvetica Portland paleo vegan wolf moon kale chips farm-to-table."
+    static let FiveComment = "Kitsch XOXO chia, leggings 3 wolf moon kale chips farm-to-table. Beard semiotics 90's banh mi."
+    static let SixComment = "Blue Bottle meggings photo booth Wes Anderson literally stumptown Vice."
+    static let SevenComment = "Food truck craft beer occupy, DIY pop-up blog drinking vinegar tousled plaid. "
+    static let EightComment = "Twee keytar chia, yr artisan sriracha +1 Godard you probably haven't heard of them."
+    static let NineComment = "Pinterest crucifix organic swag, chillwave umami banh mi forage."
+    static let TenComment = "Artisan McSweeney's fap, raw denim cray roof party gentrify distillery kale chips."
+
+    // MARK: Arrays
+
+    static let Dictionary = {}
 }
